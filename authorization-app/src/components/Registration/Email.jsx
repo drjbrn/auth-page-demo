b2a8e948cb4576034bed0ab27a469774c9f4bc84
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-02-20 11:23:28 +0200

    refactor: remove commented code

--- authorization-app/src/components/Registration/Email.jsx
+++ authorization-app/src/components/Registration/Email.jsx
@@ -1,18 +1,4 @@
-import { useState } from 'react';
-
-
 function EmailAtRegistration({ validationEmail }) {
-  // const [email, setEmail] = useState('');
-
-  // const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-  // if (regex.test(email)) {
-  //   console.log('yes, email ok')
-  // }
-
-  // const handleChange = (event) => {
-  //   setEmail(event.target.value);
-  // };
-
   return (
     <label htmlFor="email" className="registration__label form__label">
       <input
