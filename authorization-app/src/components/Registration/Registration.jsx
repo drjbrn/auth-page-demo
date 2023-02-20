4c37874229c1d03f91ca2755252320bbccdc644a
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-02-20 11:27:37 +0200

    fix: remove unnecessary import and change transition to other steps of registration

--- authorization-app/src/components/Registration/Registration.jsx
+++ authorization-app/src/components/Registration/Registration.jsx
@@ -1,31 +1,33 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
 import './Registration.scss';
 import '../../styles/_button.scss';
 import '../../styles/_form.scss';
 import SecondStepOfRegistration from './SecondStepOfRegistration';
 import FirstStepOfRegistration from './FirstStepOfRegistration';
+import RegistrationAccepted from './RegistrationAccepted';
 
 function Registration() {
   const [stepOfRegistration, setStepOfRegistration] = useState('first');
+  const [acceptedRegistration, setAcceptedRegistration] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     setStepOfRegistration('second');
   }
 
+  const handleSubmitAccepted = (event) => {
+    event.preventDefault();
+    setAcceptedRegistration(true);
+  }
+
   return (
     <div className='registration'>
       { stepOfRegistration === 'first'
         ? <FirstStepOfRegistration handleSubmit={handleSubmit} />
-        : <SecondStepOfRegistration />
+        : acceptedRegistration === false
+        ? <SecondStepOfRegistration handleSubmit={handleSubmitAccepted}/>
+        : <RegistrationAccepted />
       }
-      <div className='switch'>
-        <p>
-          Already have an account?
-        </p>
-        <Link to="../login">Login</Link>
-      </div>
     </div>
   );
 }
