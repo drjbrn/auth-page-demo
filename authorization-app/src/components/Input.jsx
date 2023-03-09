4801448db150132e2976bc7a11f19418f72c8128
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-03-09 19:04:40 +0200

    refactor: delete duplicate code

--- authorization-app/src/components/Input.jsx
+++ authorization-app/src/components/Input.jsx
@@ -17,27 +17,3 @@ const Input = ({ type, id, name, value, onChange, className }) => {
 };
 
 export default Input;
-
-// const InputField = ({ type, id, name, value, onChange, spanText, div }) => {
-//   return (
-//     <label htmlFor={id} className='form__label'>
-//       <input
-//         type={type}
-//         id={id}
-//         name={name}
-//         value={value}
-//         onChange={onChange}
-//         className='form__input'
-//         required
-//       />
-//       <div>
-//         {div}
-//       </div>
-//       <span className='form__span'>
-//         {spanText}
-//       </span>
-//     </label>
-//   );
-// };
-
-// export default InputField;
