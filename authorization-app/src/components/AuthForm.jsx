e1a24c5edc1386446f180f53a88d00b43517435a
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-03-13 14:47:50 +0200

    feat: add error message and add data information (email, password) for next component

--- authorization-app/src/components/AuthForm.jsx
+++ authorization-app/src/components/AuthForm.jsx
@@ -3,34 +3,23 @@ import Email from './Email'
 import Password from './Passwords'
 import PasswordRequirements from './PasswordRequirements/PasswordRequirements';
 
-function AuthForm({ submitForm, buttonText, renderPasswordRequirements, onAuthData, error }) {
+function AuthForm({ submitForm, buttonText, renderPasswordRequirements }) {
   const { email, password, isFormValid, handleEmailChange, handlePasswordChange } = useEmailAndPasswordValidation();
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    onAuthData(email, password);
-    submitForm();
-  };
 
   return(
     <form
       action=''
-      onSubmit={handleSubmit}
+      onSubmit={submitForm}
       className='form'>
       <Email
-        validationEmail={handleEmailChange}
-        email={email}
+          validationEmail={handleEmailChange}
+          email={email}
       />
       <Password
         validationPassword={handlePasswordChange}
         password={password}
       />
       {renderPasswordRequirements && password.length > 0 && <PasswordRequirements password={password}/>}
-      {error && <p className="form__login_error">
-                  Invalid email or password. <br />
-                  Please check your login information and try again.
-                </p>
-      }
       <button
         disabled={isFormValid}
         className='btn'>
