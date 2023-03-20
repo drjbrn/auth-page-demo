9c29eaf99dab18c44ad4414a50e508b88980cf46
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-03-20 19:54:37 +0200

    feat: add rules for correct work router on gh-pages

--- authorization-app/src/App.jsx
+++ authorization-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
@@ -8,16 +8,15 @@ function App() {
   return (
     <div className='app'>
       <div className='container'>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/registration' element={<Registration />} />
-          </Routes>
-        </BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/registration' element={<Registration />} />
+        </Routes>
       </div>
     </div>
   );
 }
 
 export default App;
+ 
--- authorization-app/src/index.jsx
+++ authorization-app/src/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import './index.scss';
 import App from './App';
+
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
